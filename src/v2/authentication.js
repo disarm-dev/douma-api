991c5abab7f2b20bea486d227e61a2b93ce8c4ed
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-05-04 12:05:05 +0200

    Add email key

--- src/v2/authentication.js
+++ src/v2/authentication.js
@@ -104,9 +104,9 @@ function getRowData(auth) {
         return;
       }
       var rows = response.values;
-      let keys = ['_id', 'username', 'password', 'permissions', 'IRS_MONITOR', 'IRS_PLAN', 'IRS_TASKER', 'IRS_RECORD', 'FOCI', 'RASTERS']
+      let keys = ['_id', 'username', 'password', 'email', 'permissions', 'IRS_MONITOR', 'IRS_PLAN', 'IRS_TASKER', 'IRS_RECORD', 'FOCI', 'RASTERS']
       let applets = ['IRS_MONITOR', 'IRS_PLAN', 'IRS_TASKER', 'IRS_RECORD', 'FOCI', 'RASTERS']
-      
+
       let users = response.values.map(row => {
         let user = {allowed_apps: {read: [], write: []}}
 
