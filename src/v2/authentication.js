e84fd333d173433585b5c2ffec2af060fb308397
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-05-12 11:49:06 +0200

    Revert "Safer and stuff"
    This reverts commit 0788ccd0ae85f51ebc272af0f31ecb73a848314b.

--- src/v2/authentication.js
+++ src/v2/authentication.js
@@ -22,18 +22,19 @@ module.exports = function authenticate (req, res) {
   let requesting_user = req.body.user
   
   getUserData().then(users => {
+    console.log(users)
     let found_user = users.find((user) => {
-      return user.username == requesting_user.username
+      return user.email == requesting_user.email
     })
 
     if (!found_user) {
-      res.status(404).send({error: 'Unknown user', username: requesting_user.username});
+      res.status(500).send({error: 'Incorrect email'});
     }
 
     if (found_user.password === requesting_user.password) {
       res.send(found_user)
     } else {
-      res.status(404).send({error: 'Unknown user', username: requesting_user.username});
+      res.status(500).send({error: 'Incorrect password'});
     }
   })
 }
