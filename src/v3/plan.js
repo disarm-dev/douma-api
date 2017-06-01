288cb297650a17c8143a00c697e8ad828d75b5c8
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-06-01 10:11:17 +0200

    Pretty much working

--- src/v3/plan.js
+++ src/v3/plan.js
@@ -2,15 +2,23 @@ module.exports = {
   get_current(db, req, res) {
     const plans = db.collection("plans")
 
-    plans.findOne((err, docs) => {
-      res.send({
-        data: docs
-      });
+    plans.find().sort({planned_at: -1}).limit(1).toArray((err, docs) => {
+      let doc = docs[0]
+      res.send(doc)
     });
   },
 
   create(db, req, res) {
     const plans = db.collection("plans")
+    
+    let doc = req.body
+    
+    doc.planned_at = new Date()
 
+    plans.insertOne(doc).then((result, err) => {
+      res.send({'status': "success"})
+    }).catch(err => {
+      res.status(403).send(err)
+    }) 
   }
 }
--- src/v3/record.js
+++ src/v3/record.js
@@ -1,13 +1,26 @@
 module.exports = {
   create(db, req, res) {
     const records = db.collection("records")
-    records.insertOne(req)
-      .then((result) => res.send(result))
-      .catch((error) => res.status(403).send(error))
+
+    let doc = req.body
+    
+    doc.recorded_at = new Date()
+
+    records.insertOne(doc).then((result) => {
+      res.send({'status': "success"})
+    })
+    .catch(err => {
+      res.status(403).send(err)
+    }) 
   },
 
   get_all(db, req, res) {
     const records = db.collection("records")
 
+    let country = req.query.country
+
+    records.find({country: country}).toArray((err, docs) => {
+      res.send(docs)
+    })
   }
 }
