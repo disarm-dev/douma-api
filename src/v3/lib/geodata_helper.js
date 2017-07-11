97e1e359a8874e43d0963d0f99d070f754672a14
Jonathan Smith <jonathan@peoplesized.com>
2017-07-11 14:03:43 +0200

    fine for now^

--- src/v3/lib/geodata_helper.js
+++ src/v3/lib/geodata_helper.js
@@ -4,21 +4,24 @@ const fetch = require('node-fetch');
 // Get instance_config from cache or from remote
 // Get geodata from cache or remote
 
-const root_url = 'localhost:8080'
-const geodata = {}
+const root_url = process.env['CLIENT_APP_URL'] || 'localhost:8080'
 
 const get_instance_config = async (instance_slug) => {
   const url = `http://${root_url}/static/instances/${instance_slug}.instance.json`
 
   try {
-    const instance_config = await fetch(url).then(res => res.json())
+    const res = await fetch(url)
+    const instance_config = await res.json()
     return instance_config
   } catch(e) {
     console.error(e)
   }
 }
 
-const get_geodata = async (instance_slug, instance_config) => {
+const get_geodata = async (instance_slug) => {
+  const geodata = {}
+  const instance_config = await get_instance_config(instance_slug)
+
   const levels = instance_config.spatial_hierarchy.levels
 
   for (level of levels) {
@@ -32,13 +35,14 @@ const get_geodata = async (instance_slug, instance_config) => {
   return geodata
 }
 
-const do_both = async () => {
+// // Test
+console.log('root_url', root_url)
+const fn = async () => {
   const instance_slug = 'bwa'
-  const instance_config = await get_instance_config(instance_slug)
-  const geodata = await get_geodata(instance_slug, instance_config)
-  console.log('geodata', Object.keys(geodata))
+  const result = await get_geodata(instance_slug)
+  console.log('geodata', Object.keys(result))
 }
 
-do_both()
+fn()
 
-// modules.export = {get_instance_config, get_geodata, do_both}
+module.exports = {get_geodata}
