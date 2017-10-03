a7d55e9fdf925b4ddd689b257d94b8d715099460
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-10-03 13:48:56 +0200

    Refactor and pray nothing breaks.

--- src/v3/routes/authentication.js
+++ src/v3/routes/authentication.js
@@ -6,7 +6,7 @@ function getUserData() {
   })
 }
 
-function formatUserPermissions (users) {
+function formatUserPermissions(users) {
   return users.map((user) => {
     user.allowed_apps = {
       read: user.read.split(',').map(t => t.toLowerCase().trim()),
@@ -18,30 +18,43 @@ function formatUserPermissions (users) {
   })
 }
 
-module.exports = function authenticate (req, res) {
-  debugger
-  const requesting_user = req.body.user
-  const instance = req.country
-  
-  getUserData().then(users => {
-    const auth_instance_user = users.find((user) => {
-      const auth_user = (user.username === requesting_user.username) && (user.password === requesting_user.password)
+function _authenticate_user({users, requesting_user, instance_slug}) {
+  const auth_instance_user = users.find((user) => {
+    const auth_user = (user.username === requesting_user.username) && (user.password === requesting_user.password)
 
-      // username and password don't match
-      if (!auth_user) return false
+    // username and password don't match
+    if (!auth_user) return false
 
-      // a dev user is authenticated against 'all' instances
-      if (user.instance_slug === 'all') return true
+    // a dev user is authenticated against 'all' instances
+    if (user.instance_slug === 'all') return true
 
-      // user is authenticated for the instance
-      if (user.instance_slug === req.country) return true
-    })
+    // user is authenticated for the instance
+    if (user.instance_slug === instance_slug) return true
+  })
+
+  delete auth_instance_user.password
+
+  return auth_instance_user
+}
+
+
+function authenticate(req, res) {
+  getUserData().then((users) => {
+    const requesting_user = req.body.user
+    const instance_slug = req.country
+
+    const auth_instance_user = _authenticate_user({users, requesting_user, instance_slug})
 
     if (auth_instance_user) {
-      delete auth_instance_user.password
       res.send(auth_instance_user)
     } else {
       res.status(401).send({error: 'Unknown user'});
     }
   })
+}
+
+
+module.exports = {
+  authenticate,
+  _authenticate_user
 }
