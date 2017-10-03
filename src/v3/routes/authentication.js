971ef3af431cde657f4851218357c74ce92cdc80
Jonathan Smith <jonathan@peoplesized.com>
2017-10-03 16:18:21 +0200

    more test

--- src/v3/routes/authentication.js
+++ src/v3/routes/authentication.js
@@ -1,11 +1,5 @@
 const getCSV = require('get-csv');
 
-function getUserData() {
-  return getCSV(process.env.SHEETS_URL).then((users) => {
-    return users.map(u => formatUserPermissions(u))
-  })
-}
-
 function formatUserPermissions(user) {
   if (!user.hasOwnProperty('read') || !user.hasOwnProperty('write')) return {}
 
@@ -38,19 +32,20 @@ function _authenticate_user({users, requesting_user, instance_slug}) {
 }
 
 
-function authenticate(req, res) {
-  getUserData().then((users) => {
-    const requesting_user = req.body.user
-    const instance_slug = req.country
+async function authenticate(req, res) {
+  const users = await getCSV(process.env.SHEETS_URL)
 
-    const auth_instance_user = _authenticate_user({users, requesting_user, instance_slug})
+  const requesting_user = req.body.user
+  const instance_slug = req.country
 
-    if (auth_instance_user) {
-      res.send(auth_instance_user)
-    } else {
-      res.status(401).send({error: 'Unknown user'});
-    }
-  })
+  const auth_instance_user = _authenticate_user({users, requesting_user, instance_slug})
+
+  if (auth_instance_user) {
+    const formatted_user = formatUserPermissions(auth_instance_user)
+    res.send(formatted_user)
+  } else {
+    res.status(401).send({error: 'Unknown user'});
+  }
 }
 
 
