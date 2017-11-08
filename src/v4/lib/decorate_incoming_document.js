fcdf0c3b5fe3d8f9174a0ba89742fca2c7a4383f
Nicolai Davies <nicolaidavies.nd@gmail.com>
2017-11-08 09:29:21 +0200

    Don't add user to plan

--- src/v4/lib/decorate_incoming_document.js
+++ src/v4/lib/decorate_incoming_document.js
@@ -1,5 +1,4 @@
 const decorate_incoming_document = ({doc, req}) => {
-  doc.user = req.user
   doc.personalised_instance_id = req.personalised_instance_id
   doc.updated_at = (+new Date())
   return doc
