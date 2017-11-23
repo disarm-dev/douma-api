b9722b14adc20df72822fd088a91ae8b8d0b5b7c
Jonathan Smith <jonathan@peoplesized.com>
2017-11-23 09:27:11 +0200

    add app.use regex testing for version prefix, remove_version_prefix from addPermission paths & openPaths

--- src/v5/index.js
+++ src/v5/index.js
@@ -1,12 +1,12 @@
-const User = require('./lib/auth')
-const addPermission = User.addPermission
+const User_v5 = require('./lib/auth')
+const addPermission = User_v5.addPermission
 
 const login = require('./routes/login')
 const plan = require('./routes/plan')
 const record = require('./routes/record')
 const assignment_plan = require('./routes/assignment_plan')
 
-User.updateUserList()
+User_v5.updateUserList()
 
 module.exports = function (app, version) {
   const version_prefix = '/' + version
@@ -15,43 +15,44 @@ module.exports = function (app, version) {
     return version_prefix + url
   }
 
-  app.use(User.authMiddleware)
-  app.use(User.endpointPermissionsMiddleware)
-  app.use(User.optionsMiddleware)
+  const version_path_regex = new RegExp(version_prefix)
+  app.use(version_path_regex, User_v5.authMiddleware)
+  app.use(version_path_regex, User_v5.endpointPermissionsMiddleware)
+  app.use(version_path_regex, User_v5.optionsMiddleware)
 
   // Meta
-  addPermission('get', v(''), ['*'])
+  addPermission('get', '/', ['*'])
   app.get(v(''), (req, res) => res.send({
     DOUMA_API: process.env.SOURCE_VERSION || 'DEV',
     version: version
   }))
 
-  addPermission('get', v('/refresh_users'), ['*'])
-  app.get(v('/refresh_users'), User.forceUpdateUserList)
+  addPermission('get', '/refresh_users', ['*'])
+  app.get(v('/refresh_users'), User_v5.forceUpdateUserList)
 
   // Auth
-  addPermission('post', v('/login'), ['*'])
+  addPermission('post', '/login', ['*'])
   app.post(v('/login'), login.login)
 
   // Plan
-  addPermission('get', v('/plan/current'), ['read:irs_plan', 'read:irs_monitor', 'read:irs_tasker'])
+  addPermission('get', '/plan/current', ['read:irs_plan', 'read:irs_monitor', 'read:irs_tasker'])
   app.get(v('/plan/current'), plan.get_current)
 
-  addPermission('post', v('/plan/create'), ['write:irs_plan'])
+  addPermission('post', '/plan/create', ['write:irs_plan'])
   app.post(v('/plan/create'), plan.create)
 
   // Record
-  addPermission('get', v('/record/all'), ['read:irs_record_point', 'read:irs_monitor'])
+  addPermission('get', '/record/all', ['read:irs_record_point', 'read:irs_monitor'])
   app.get(v('/record/all'), record.get_all)
-  addPermission('post', v('/record/updates'), ['read:irs_record_point', 'read:irs_monitor'])
+  addPermission('post', '/record/updates', ['read:irs_record_point', 'read:irs_monitor'])
   app.post(v('/record/updates'), record.get_updates)
-  addPermission('post', v('/record/create'), ['write:irs_record_point'])
+  addPermission('post', '/record/create', ['write:irs_record_point'])
   app.post(v('/record/create'), record.create)
 
   // AssignmentPlan
-  addPermission('get', v('/assignment_plan/current'), ['read:irs_tasker'])
+  addPermission('get', '/assignment_plan/current', ['read:irs_tasker'])
   app.get(v('/assignment_plan/current'), assignment_plan.read)
-  addPermission('post', v('/assignment_plan/create'), ['write:irs_tasker'])
+  addPermission('post', '/assignment_plan/create', ['write:irs_tasker'])
   app.post(v('/assignment_plan/create'), assignment_plan.create)
 
 }
