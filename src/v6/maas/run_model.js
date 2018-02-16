56d6cd237c9cec37b227424b7d65f0bc32eec32f
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-02-16 10:33:00 +0200

    Run model

--- src/v6/maas/run_model.js
+++ src/v6/maas/run_model.js
@@ -1,18 +1,14 @@
 const input = require('./input')
-const config = require('./model')
-const {validate_input,validate_output} = require('./validation')
+const {validate_input, validate_output} = require('./validation')
 
-const run_model = async ({input, config}) => {
+module.exports = async ({input,config}) => {
     try {
         let validation = validate_input(input, config)
         result = await config.fn(input) // TODO validate Output
-        //validate_output(result, config)
+        validate_output(result, config)
         return result
     } catch (e) {
         throw(e)
     }
 }
 
-module.exports = {
-    run_model
-}
