3552302ccec29af39516b2d491b4e9b210bde57f
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-03-21 10:53:08 +0200

    Debugging create plan

--- src/v6/lib/decorate_incoming_document.js
+++ src/v6/lib/decorate_incoming_document.js
@@ -1,6 +1,8 @@
 const decorate_incoming_document = ({doc, req}) => {
+    console.log('Incoming Document',doc)
   doc.personalised_instance_id = req.personalised_instance_id
   doc.updated_at = (+new Date())
+    console.log('Decorated Document',doc)
   return doc
 }
 
