d122d63fc09ada9420e881ef9d1efa042db928fe
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-03-27 15:47:54 +0200

    constants definitions for collection names

--- src/v7/lib/collections.js
+++ src/v7/lib/collections.js
@@ -1,6 +1,11 @@
 module.exports =  {
     CASE_LOCATIONS:"case_location",
-    CASE_CLUSTERS:"case_cluster"
+    CASE_CLUSTERS:"case_cluster",
+    ASSIGNMENT_PLAN:'assignement_pan',
+    CONFIG:'config',
+    GEODATA:'geodata',
+    PLAN:'plans',
+    RECORD:'records'
 }
 
 //I intentionably left the collection names in singular, mongodb has this magical thing of converting to plural
