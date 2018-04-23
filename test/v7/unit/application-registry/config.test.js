a6c5894f14390422b36d942fed4e2ee8c69a578a
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-04-23 09:37:48 +0200

    Test permissions for creating config

--- test/v7/unit/application-registry/config.test.js
+++ test/v7/unit/application-registry/config.test.js
@@ -6,13 +6,30 @@ const body_parser = require('body-parser')
 const test = require('ava').test
 const collections = require('../../../../src/v7/lib/collections')
 
+const novice_key = '04a184f1adf9b44a065d287a5d377284'
+const admin_key  = '27599f876ad55a65762b2b9b57f1ba31'
+
 test.serial('GET /v7/config config get is open => 200', async t => {
     t.plan(1)
     const res = await request(app).get('/v7/config?country=swz')
     t.is(res.status, 200)
 })
 
-test.skip('Create A new Config ' , async t => {
+test.serial('Create A new Config ' , async t => {
+    const bwa_config = require('../../../bwa-config')
+
     const res = await request(app).post('/v7/config?country=all')
+        .set('Api-Key', admin_key)
+        .send({config_data:bwa_config})
+
     t.is(res.status, 201)
+})
+
+test.serial('Config creation fails with 401 ' , async t => {
+    const bwa_config = require('../../../bwa-config')
+
+    const res = await request(app).post('/v7/config?country=all')
+        .set('Api-Key', novice_key)
+        .send({config_data:bwa_config})
+    t.is(res.status, 401)
 })
