c5cc8a8f17fc3a10885b2df90da08074e268ce1e
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-06-22 11:47:23 +0200

    Seasons test

--- test/v7/intrgration/seasons.test.js
+++ test/v7/intrgration/seasons.test.js
@@ -7,30 +7,37 @@ const test = require('ava').test
 //const collections = require('../../../../src/v7/lib/collections')
 const {tear_down, populate_responses, keys} = require('../helper')
 
-const populate_config = async () => {
-    const bwa_config = require('../../bwa-config')
+const send_config = async (cfg) => {
 
     const res = await request(app).post('/v7/config?country=all')
         .set('Api-Key', keys.admin_key)
-        .send({config_data: bwa_config})
-    console.log(res.body);
+        .send(cfg)
+    console.log(res.body)
 }
 
 
 test.beforeEach(async () => {
-    await tear_down()
-    await populate_config()
-    await populate_responses()
+    tear_down()
 })
 
 
 test.serial('Update Seasons', async t => {
+    const bwa_config = require('../../bwa-config')
+    const initial_length = bwa_config.config_data.applets.irs_monitor.season_start_dates.length;
+
+    await send_config(bwa_config)
     //get config
     const res = await request(app).get('/v7/config/bwa?country=all')
 
     //Add A date on the seasons
     let config = res.body;
-    console.log('Seasons',config/*.applets.irs_monitor.season_start_dates*/)
+    config.applets.irs_monitor.season_start_dates.push('2018-04-05')
+    await send_config({config_data:config})
 
-    t.pass();
+    const update =  await request(app).get('/v7/config/bwa?country=all')
+    const updated_config = update.body;
+
+    const updated_length = updated_config.applets.irs_monitor.season_start_dates.length;
+
+    t.is(initial_length,updated_length-1);
 })
