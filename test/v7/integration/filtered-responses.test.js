4f90bdea86acc33cbc63901376e10ddfb13753f7
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-07-03 17:01:19 +0200

    Enabled filtered responses test

--- test/v7/integration/filtered-responses.test.js
+++ test/v7/integration/filtered-responses.test.js
@@ -10,14 +10,13 @@ const test = require('ava').test
 //Insert seed responses
 test.beforeEach(async () => {
     await tear_down()
-  const populated = await populate_responses()
-  console.log('Populated', populated)
+  await populate_responses()
 })
 
 
 //test('Test For creating responses')
 
-test.skip('Get filtered responses', async t => {
+test('Get filtered responses', async t => {
     t.plan(2)
     const res = await request(app).get('/v7/record/filtered?country=bwa&start_date=2018-02-20T09:39:45.347Z&end_date=2018-03-27T08:11:58.410Z').set('Api-Key', admin_key)
   t.is(res.status, 200)
