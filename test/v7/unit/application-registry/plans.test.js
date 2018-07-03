c3508e6718eb6e44f4763d66f7e679afc3ffb8d4
Sihle Mbhamali <mbhamalimosaic@gmail.com>
2018-07-03 16:43:56 +0200

    Reduced plan targets for test plans

--- test/v7/unit/application-registry/plans.test.js
+++ test/v7/unit/application-registry/plans.test.js
@@ -24,51 +24,7 @@ const plan = {
         "estimated_rooms": 3200,
         "assigned_to_team_name": null,
         "id": 107
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 183}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 192
-    }, {"estimated_rooms": 10593, "assigned_to_team_name": null, "id": 239}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 247
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 252}, {
-        "estimated_rooms": 759,
-        "assigned_to_team_name": null,
-        "id": 263
-    }, {"estimated_rooms": 325, "assigned_to_team_name": null, "id": 267}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 269
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 270}, {
-        "estimated_rooms": 1530,
-        "assigned_to_team_name": null,
-        "id": 358
-    }, {"estimated_rooms": 3631, "assigned_to_team_name": null, "id": 377}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 388
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 393}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 394
-    }, {"estimated_rooms": 90, "assigned_to_team_name": null, "id": 412}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 456
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 472}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 474
-    }, {"estimated_rooms": 650, "assigned_to_team_name": null, "id": 478}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 501
-    }, {"estimated_rooms": 795, "assigned_to_team_name": null, "id": 540}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 552
-    }, {"estimated_rooms": 1272, "assigned_to_team_name": null, "id": 798}],
+    }],
     "country": "bwa",
     "id": "fd1e7b6d-8ed0-4be6-b69f-dc06d84791f5"
 }
@@ -85,51 +41,7 @@ const second_plan = {
         "estimated_rooms": 3200,
         "assigned_to_team_name": null,
         "id": 107
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 183}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 192
-    }, {"estimated_rooms": 10593, "assigned_to_team_name": null, "id": 239}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 247
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 252}, {
-        "estimated_rooms": 759,
-        "assigned_to_team_name": null,
-        "id": 263
-    }, {"estimated_rooms": 325, "assigned_to_team_name": null, "id": 267}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 269
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 270}, {
-        "estimated_rooms": 1530,
-        "assigned_to_team_name": null,
-        "id": 358
-    }, {"estimated_rooms": 3631, "assigned_to_team_name": null, "id": 377}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 388
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 393}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 394
-    }, {"estimated_rooms": 90, "assigned_to_team_name": null, "id": 412}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 456
-    }, {"estimated_rooms": 0, "assigned_to_team_name": null, "id": 472}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 474
-    }, {"estimated_rooms": 650, "assigned_to_team_name": null, "id": 478}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 501
-    }, {"estimated_rooms": 795, "assigned_to_team_name": null, "id": 540}, {
-        "estimated_rooms": 0,
-        "assigned_to_team_name": null,
-        "id": 552
-    }, {"estimated_rooms": 1272, "assigned_to_team_name": null, "id": 798}],
+    }],
     "country": "bwa",
     "id": "fd1e7b6d-8ed0-4be6-b69f-dc06d84791f5"
 }
